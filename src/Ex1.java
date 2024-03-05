5890b014b8a13717e57a20eaf46186a372b03feb
Yanis3Pique <pavel-yanis.popescu@s.unibuc.ro>
2024-03-05 17:32:23 +0200

    Ex1

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Ex1 {
     public static void main(String[] args) {
 //        1 Scrieți un program Java care afișează numerele divizibile cu 3 de la 1 la 99.
         for(int i = 3; i <= 99; i = i + 3) {
---
+++ src/Ex2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int nr1 = scanner.nextInt();
+        int nr2 = scanner.nextInt();
+        int nr3 = scanner.nextInt();
+
+        if(nr1 < nr2) {
+            if(nr2 < nr3) {
+                System.out.println(nr1 + " < " + nr2 + " < " + nr3);
+            } else if (nr2 > nr3) {
+                System.out.println(nr1 + " < " + nr2 + " > " + nr3);
+            }
+        }
+        else {
+            if(nr2 < nr3) {
+                System.out.println(nr1 + " > " + nr2 + " < " + nr3);
+            } else if (nr2 > nr3) {
+                System.out.println(nr1 + " > " + nr2 + " > " + nr3);
+            }
+        }
+    }
+}
